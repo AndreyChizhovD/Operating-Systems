51fe1753cfac2dd20f25fdc5f4c83325caf41176
AndreyChizhovD <92399109+AndreyChizhovD@users.noreply.github.com>
2023-03-16 21:18:23 +0300

    Update run.sh

--- hw7/run.sh
+++ hw7/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-gcc ~/osi/sem7/hw/1.c; mv a.out 1.out
-gcc ~/osi/sem7/hw/2.c; mv a.out 2.out
+gcc 1.c; mv a.out 1.out
+gcc 2.c; mv a.out 2.out
 ./1.out & ./2.out && fg
 rm 1.out 2.out
